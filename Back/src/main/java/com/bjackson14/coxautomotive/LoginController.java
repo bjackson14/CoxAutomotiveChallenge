98cd59bbe9aab06edf97262b8cae852a45ed648f
bjackson14 <ben.jksn@gmail.com>
2019-07-10 13:51:16 -0400

    Added exception handling

--- Back/src/main/java/com/bjackson14/coxautomotive/LoginController.java
+++ Back/src/main/java/com/bjackson14/coxautomotive/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class LoginController {
@@ -12,7 +13,7 @@ public class LoginController {
     private LoginDAO loginDAO;
 
     @RequestMapping(value = "/getLogin/{username}", method = RequestMethod.GET)
-    public Login getLogin(@PathVariable String username) {
+    public Login getLogin(@PathVariable String username) throws ResponseStatusException {
         return loginDAO.getLoginByUsername(username);
     }
 }
--- Back/src/main/java/com/bjackson14/coxautomotive/LoginDAO.java
+++ Back/src/main/java/com/bjackson14/coxautomotive/LoginDAO.java
@@ -1,6 +1,8 @@
 package com.bjackson14.coxautomotive;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,7 +15,10 @@ public class LoginDAO {
     @PersistenceContext
     private EntityManager em;
 
-    public Login getLoginByUsername(String username) {
+    public Login getLoginByUsername(String username) throws ResponseStatusException {
+        Login login = em.find(Login.class, username);
+        if (login == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Username Provided Does Not Exits");
         return em.find(Login.class, username);
     }
 }
