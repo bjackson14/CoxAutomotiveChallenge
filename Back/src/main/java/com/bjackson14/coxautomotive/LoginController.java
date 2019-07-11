f9e551f8534847400063c419cf4990d5c0831ef2
bjackson14 <ben.jksn@gmail.com>
2019-07-11 18:05:15 -0400

    Set up cors on backend

--- Back/src/main/java/com/bjackson14/coxautomotive/LoginController.java
+++ Back/src/main/java/com/bjackson14/coxautomotive/LoginController.java
@@ -1,10 +1,7 @@
 package com.bjackson14.coxautomotive;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -12,6 +9,7 @@ public class LoginController {
     @Autowired
     private LoginDAO loginDAO;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/getLogin/{username}", method = RequestMethod.GET)
     public Login getLogin(@PathVariable String username) throws ResponseStatusException {
         return loginDAO.getLoginByUsername(username);
